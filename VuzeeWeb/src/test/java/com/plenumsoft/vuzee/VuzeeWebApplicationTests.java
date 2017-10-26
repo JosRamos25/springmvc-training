bd0972b712edcd82e01eb929edab6c5f99b115c0
jramos <josramos25@gmail.com>
2017-10-25 19:00:10 -0500

    se agrega test de controllers

--- VuzeeWeb/src/test/java/com/plenumsoft/vuzee/VuzeeWebApplicationTests.java
+++ VuzeeWeb/src/test/java/com/plenumsoft/vuzee/VuzeeWebApplicationTests.java
@@ -1,16 +1,25 @@
 package com.plenumsoft.vuzee;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.plenumsoft.vuzee.api.controllers.CandidatesRestController;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class VuzeeWebApplicationTests {
 
+	@Autowired
+	private CandidatesRestController candidatesRestController;
+
 	@Test
 	public void contextLoads() {
+		assertThat(candidatesRestController).isNotNull();
 	}
 
 }
